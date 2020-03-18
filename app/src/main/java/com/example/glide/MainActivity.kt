6df8c1614b27b921506b4daa258f16af1c1a78f6
李宜雯 <liy721511@gmail.com>
2020-03-18 16:44:54 +0800

    first commit

---
+++ app/src/main/java/com/example/glide/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.glide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Glide.with(this).load(R.drawable.kolara).into(imageView2)
+
+    }
+}
